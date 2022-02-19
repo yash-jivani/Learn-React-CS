ad91ee5160643da36859a5773aa58803fca3c767
yash-jivani <yashjivani17@gmail.com>
2022-02-19 16:49:04 +0530

    component-intro

---
+++ src/Cls-comp.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+export default class ClsComp extends Component{
+    render(){
+        return (
+            <div>
+                <h1>this is class comp</h1>
+            </div>
+        )
+    }
+}
---
+++ src/User.js
@@ -0,0 +1,12 @@
+// function comp.
+
+function User(){
+    return (
+        <div>
+            <h1>hello world !</h1>
+            <h2>This is react comp | function comp</h2>
+        </div>
+    )
+}
+
+export default User;
