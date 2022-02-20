b0a3f4d58ff8cffad769ec2ae9c539933bd63fae
yash-jivani <yashjivani17@gmail.com>
2022-02-20 20:16:16 +0530

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import FuncAsProps from './component/FuncAsProps'
 
-const parentFunc = () => {
-  alert('Parent Function')
-  console.log('Function from parent (App.js)')
+/*
+
+===== componentDidMount =====
+
+? jab HTMl comp ready hota hai uske bad componentDidMount call hota hai
+? use : with APIs
+* HTML comp ready hone ke bad render method more thn 1 time call kar skte hai lekin componentDidMount sirf ek bar hi call hota hai
+
+*/
+
+import React from "react";
+
+export default class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state={
+      name:'yash'
+    }
+    console.log('constructor called')
+  }
+
+  componentDidMount(){
+    console.log('componentDid Mount called')
+  }
+
+  render(){
+    console.log('render called')
+    return(
+      <div className="container">
+        <h2>ComponentDidMount with Class Component</h2>
+        <h3>{this.state.name}</h3>
+        <button onClick={()=>{this.setState({name:'yash-jivani'})}}>click</button>
+      </div>
+    )
+  }
 }
 
-export default function App() {
-  return (
-    <div className='container'>
-      <FuncAsProps funcX = {parentFunc} />
-    </div>
-  )
-}
