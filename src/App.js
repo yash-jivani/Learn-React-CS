bf9bc59505efe650a53a0ef5b59dfb7934a6252c
yash-jivani <yashjivani17@gmail.com>
2022-02-21 17:35:19 +0530

    useEffect with condition 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 /*
 
-useEffect :  componentDidMount, componentDidUpdate, and componentWillUnmount combined.
-useEffect parameter me ek function leta hai
+useEffect with conditions (call useEffect for specific data/state)
+
 */
 
-import React,{ useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function App() {
-  const [count, setcount] = useState(0)
-  useEffect( ()=>{
-    console.log('useEffect called')
-  } )
-  return (
 
+  const [one, setone] = useState(1)
+  const [two, settwo] = useState(10)
+
+  // ? called for both (when state chnage)
+  useEffect(() => {
+    console.log('useEffect called UNI');
+  })  // with no conditions
+
+  // ? jab one ki state chng hogi sirf tab call hoga
+  useEffect(() => {
+    console.log('ONE')
+  }, [one])  // with condition
+
+  // ? jab two ki state chng hogi sirf tab call hoga
+  useEffect(() => {
+    console.log('TWO')
+  }, [two])  // with conditions
+
+  return (
     <div className='container'>
 
-      <h1>useEffect-1</h1>
-      <h3>{count}</h3>
-      <button onClick={()=>{setcount(count+1)}}>click</button>
+      <h1>useEffect with conditions</h1>
+      <h3>{one}</h3>
+      <h3>{two}</h3>
+
+      <button onClick={() => { setone(one + 1) }}>One</button>
+      <button onClick={() => { settwo(two + 1) }}>Two</button>
 
     </div>
   )
 }
+
