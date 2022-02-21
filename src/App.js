319e1b443eb911766cc97d82869b6e16b1aeb79a
yash-jivani <yashjivani17@gmail.com>
2022-02-21 18:31:21 +0530

    install/demo React-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-
-/*
-
-3 ways to add css :
-1) with normal css file 
-2) with inline css
-3) with modular css file
-
-*/
-
 import React from 'react'
-import "./style.css"  
-import style from "./custom.module.css"
+import {Button,Alert} from 'react-bootstrap'
 
 export default function App() {
   return (
     <div className='container'>
-
-      {/* 1. with normal css file */}
-      <h1 className='heading1'>With Style.css</h1>
-
-      {/* 2. with inline css */}
-      <h1 style={{background:"green",color:'white'}}>With inline css</h1>
-
-      {/* 3. with modular file | sytx : {style.className} */}
-      <h1 className={style.edit}>With modular file</h1>
-
+    <h1>React-Bootstrap</h1>
+    <Button variant="danger">Danger Btn</Button>
+    <Alert variant='success'>This is success Alert</Alert>
     </div>
   )
 }
