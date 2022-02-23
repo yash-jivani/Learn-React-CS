2e37e42b77eb5f9421595bed9024908760bb577b
yash-jivani <yashjivani17@gmail.com>
2022-02-23 20:34:41 +0530

    DELETE Method

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
-import React, { useState } from 'react'
-
-// data ko server me create/add karne ke liye post method ka use hota hai 
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [number, setNumber] = useState("")
+  const [dataArr, setDataArr] = useState([])
+  let URL = `http://localhost:5000/users`;
 
-  const saveToApi = () => {
-    // console.log(name,email,number)
-    let data = { name, email, number }
-    // console.log(data)
-    const URL = `http://localhost:5000/users`;
+  useEffect(() => {
+    getUsers()
+    }, [])
 
-    // * sytx : for adding json data to server
-    fetch(URL, {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body:JSON.stringify(data)
+  function getUsers(){
+
+    fetch(URL)
+      .then((response) => { return response.json() })
+      .then((data) => { setDataArr(data) })
+  }
+
+  console.log(dataArr);
+
+  const deleteUser = (id) => {
+    URL = `http://localhost:5000/users/${id}`
+    alert(id)
+    fetch(URL,{
+      method:"DELETE"
     })
+    // getUsers()
+    
   }
 
   return (
     <div className='container'>
 
-      <h1>Post API Example</h1>
+      <h1>DELETE Method</h1>
+      <span> - Refresh page after delete - </span>
+      {
+        dataArr.map((user, i) => {
+          return (
+            <div key={i}>
+              <h5>__________________</h5>
+              <h2>{user.name}, {user.email}, {user.number}</h2>
+              <button onClick={()=>{deleteUser(user.id)}}>DELETE</button>
+            </div>
+          )
+        })
+      }
 
-      <input type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="Name" /> <br /> <br />
-      <input type="text" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="Email" /> <br /> <br />
-      <input type="text" value={number} onChange={(e) => { setNumber(e.target.value) }} placeholder="Number" /> <br /> <br />
-      <button onClick={saveToApi}>Save to API</button>
     </div>
   )
 }
